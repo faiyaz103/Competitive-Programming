83240563d610cc9660e5ee107fe7ef56d104aeed
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-08-16 00:46:33 +0600

    Code Added

---
+++ Number-Theory/Euclid-Division-Algo(GCD)/euclid_division.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Gcd(int a, int b){
+
+    if(b==0){
+        return a;
+    }
+    else{
+        return Gcd(b,a%b);
+    }
+}
+
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+
+    cout<<Gcd(a,b)<<endl;
+
+    return 0;
+}
